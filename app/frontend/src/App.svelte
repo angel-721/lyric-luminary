ee0017a12bda265097b24d1c19cb23059889d701
angel-721 <angelvelasquez2002@gmail.com>
2024-03-14 19:39:23 -0600

    Get started with styling

--- app/frontend/src/App.svelte
+++ app/frontend/src/App.svelte
@@ -1,6 +1,8 @@
 <script>
   import {Predict} from '../wailsjs/go/main/App.js'
   import {GetSpotifyRecommendations} from '../wailsjs/go/main/App.js'
+	import '@picocss/pico'
+
 
 	let predictedGenre = ""
 	let songLyrics = ""
@@ -32,22 +34,39 @@
 
 </script>
 
-<main>
-	<h1>Hi</h1>
-	<textarea bind:value={songLyrics} placeholder="Enter Song Lyrics Here"
-	rows="30" cols="30"/>
+<main class="container">
+
+	<h1 class="centered-text">Music Genre Classifier</h1>
+
+	<div class="centered-div">
+		<textarea bind:value={songLyrics} placeholder="Enter Song Lyrics Here"
+		rows="15" id="lyric-text-area" />
+	</div>
+
 	<button on:click={predict}>Predict!</button>
 	<button on:click={fetchRecommendations}>Get Spotify Recommendations</button>
 	<p>{predictedGenre}</p>
 
 	<h2>Recommendations</h2>
 	{#each recommendations as song}
-		<p>{song.track_name} by {song.artist_name}</p>
-		<img src={song.image_link} alt="">
+		<div>
+			<p>{song.track_name} by {song.artist_name}</p>
+			<img src={song.image_link} alt="">
+		</div>
 	{/each}
-
 </main>
 
+<input type="checkbox" role="switch" />
+
 <style>
+.centered-text{
+	text-align: center;
+}
+
+#lyric-text-area{
+	height: 100%;
+	width: 70%;
+	text-align: center;
+}
 
 </style>
