228319c446e03202c8ed036b5079d4a12ca47b34
angel-721 <angelvelasquez2002@gmail.com>
2024-03-14 18:33:59 -0600

    Show spotify images on front end

--- app/frontend/src/App.svelte
+++ app/frontend/src/App.svelte
@@ -4,7 +4,7 @@
 
 	let predictedGenre = ""
 	let songLyrics = ""
-	let recs = ""
+	let recommendations = []
 
 	function predict(){
 		if(songLyrics == ""){
@@ -26,7 +26,7 @@
 		}
 		GetSpotifyRecommendations(predictedGenre).then(result => {
 			const recommendedTracks = JSON.parse(result);
-			recs = JSON.stringify(recommendedTracks)
+			recommendations = recommendedTracks
 		})
 	}
 
@@ -39,8 +39,13 @@
 	<button on:click={predict}>Predict!</button>
 	<button on:click={fetchRecommendations}>Get Spotify Recommendations</button>
 	<p>{predictedGenre}</p>
-	<p>Your recs:</p>
-	<p>{recs}</p>
+
+	<h2>Recommendations</h2>
+	{#each recommendations as song}
+		<p>{song.track_name} by {song.artist_name}</p>
+		<img src={song.image_link} alt="">
+	{/each}
+
 </main>
 
 <style>
