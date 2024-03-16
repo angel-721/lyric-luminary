472be5524b19ffe09a1c8268246b51eaf1f143cc
angel-721 <angelvelasquez2002@gmail.com>
2024-03-16 00:18:02 -0600

    Git parse lyrics working on new data

--- scripts/parse_lryics.nim
+++ scripts/parse_lryics.nim
@@ -1,63 +1,98 @@
 import std/parsecsv
 import std/strformat
-
+import os
 
 proc get_lyrics(): void =
-  var parser: CsvParser
-  parser.open("../en_lyrics.csv")
-  parser.readHeaderRow()
+    var parser: CsvParser
+    parser.open("../data/trainp.csv")
+    parser.readHeaderRow()
 
-  var rap_i: int = 1
-  var rock_i: int = 1
-  var pop_i: int = 1
-  var country_i: int = 1
-  var misc_i: int = 1
-  var count: int = 1
+    var rock_i: int = 1
+    var metal_i: int = 1
+    var pop_i: int = 1
+    var indie_i: int = 1
+    var rb_i: int = 1
+    var folk_i: int = 1
+    var electronic_i: int = 1
+    var jazz_i: int = 1
+    var hiphop_i: int = 1
+    var country_i: int = 1
 
-  var fName: string
-  var label: string
-  var tag: string
+    var count: int = 1
+    var fName: string
+    var label: string
+    var genre: string
 
-  while parser.readRow():
+    while parser.readRow():
+        fName = ""
+        label = ""
+        genre = parser.rowEntry("genre")
 
-    fName = ""
-    label = ""
-    tag = parser.rowEntry("tag")
+        if genre == "Rock":
+            label = "rock"
+            fName.add(fmt"../data/train/{label}/")
+            fName.add(fmt"file_{rock_i}.txt")
+            rock_i += 1
+        elif genre == "Metal":
+            label = "metal"
+            fName.add(fmt"../data/train/{label}/")
+            fName.add(fmt"file_{metal_i}.txt")
+            metal_i += 1
+        elif genre == "Pop":
+            label = "pop"
+            fName.add(fmt"../data/train/{label}/")
+            fName.add(fmt"file_{pop_i}.txt")
+            pop_i += 1
+        elif genre == "Indie":
+            label = "indie"
+            fName.add(fmt"../data/train/{label}/")
+            fName.add(fmt"file_{indie_i}.txt")
+            indie_i += 1
+        elif genre == "R&B":
+            label = "r&b"
+            fName.add(fmt"../data/train/{label}/")
+            fName.add(fmt"file_{rb_i}.txt")
+            rb_i += 1
+        elif genre == "Folk":
+            label = "folk"
+            fName.add(fmt"../data/train/{label}/")
+            fName.add(fmt"file_{folk_i}.txt")
+            folk_i += 1
+        elif genre == "Electronic":
+            label = "electronic"
+            fName.add(fmt"../data/train/{label}/")
+            fName.add(fmt"file_{electronic_i}.txt")
+            electronic_i += 1
+        elif genre == "Jazz":
+            label = "jazz"
+            fName.add(fmt"../data/train/{label}/")
+            fName.add(fmt"file_{jazz_i}.txt")
+            jazz_i += 1
+        elif genre == "Hip-Hop":
+            label = "hip-hop"
+            fName.add(fmt"../data/train/{label}/")
+            fName.add(fmt"file_{hiphop_i}.txt")
+            hiphop_i += 1
+        elif genre == "Country":
+            label = "country"
+            fName.add(fmt"../data/train/{label}/")
+            fName.add(fmt"file_{country_i}.txt")
+            country_i += 1
+        else:
+            echo "Genre not found: ", genre
 
-    if tag == "pop":
-      label = "pop"
-      fName.add(fmt"./{label}/")
-      fName.add(fmt"file_{pop_i}.txt")
-      pop_i += 1
-    elif tag == "rock":
-      label = "rock"
-      fName.add(fmt"./{label}/")
-      fName.add(fmt"file_{rock_i}.txt")
-      rock_i += 1
-    elif tag == "misc":
-      label = "misc"
-      fName.add(fmt"./{label}/")
-      fName.add(fmt"file_{misc_i}.txt")
-      misc_i += 1
-    elif tag == "rap":
-      label = "rap"
-      fName.add(fmt"./{label}/")
-      fName.add(fmt"file_{rap_i}.txt")
-      rap_i += 1
-    elif tag == "country":
-      label = "country"
-      fName.add(fmt"./{label}/")
-      fName.add(fmt"file_{country_i}.txt")
-      country_i += 1
-    else:
-      echo "Tag not found: ", tag
+        writeFile(fName, parser.rowEntry("lyrics"))
+        echo "Song: ", count, " genre: ", genre
+        count += 1
 
-    writeFile(fName, parser.rowEntry("lyrics"))
-    echo "Song: ", count, " Tag: ", tag
-    count += 1
+    parser.close()
 
-  parser.close()
+let genres = ["rock", "metal", "pop", "indie", "r&b", "folk", "electronic",
+        "jazz", "hip-hop", "country"]
 
+for genre in genres:
+    if not dirExists(fmt"../data/train/{genre}/"):
+        echo fmt"Making ../data/train/{genre} dir"
+        createDir(fmt"../data/train/{genre}/")
 
 get_lyrics()
-
